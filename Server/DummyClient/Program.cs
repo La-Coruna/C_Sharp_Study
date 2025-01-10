0cdb7f0d9ede77f7ed04c85a77e31bece74bbaa8
Park Jongyun <65222636+La-Coruna@users.noreply.github.com>
2025-01-10 18:25:24 +0900

    1. 간단한 소켓프로그래밍
    간단한 소켓 프로그래밍

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace DummyClient
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // DNS (Domain Name System) 정보 가져오기
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 클라이언트 소켓 생성
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 서버 연결 시도
+                socket.Connect(endPoint);
+                Console.WriteLine($"서버 {socket.RemoteEndPoint.ToString()}에 연결됨");
+
+                // 메시지 전송
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                socket.Send(sendBuff);
+
+                // 서버 응답 수신
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 연결 종료
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"클라이언트 오류: {e}");
+            }
         }
     }
 }
