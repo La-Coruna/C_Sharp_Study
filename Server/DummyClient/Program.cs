deb2c8e7fc090137f3e8d8a2ef4899464ffa4776
Park Jongyun <65222636+La-Coruna@users.noreply.github.com>
2025-01-13 12:49:52 +0900

    Listener

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -14,32 +14,37 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 클라이언트 소켓 생성
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            try
+            while (true)
             {
-                // 서버 연결 시도
-                socket.Connect(endPoint);
-                Console.WriteLine($"서버 {socket.RemoteEndPoint.ToString()}에 연결됨");
+                // 클라이언트 소켓 생성
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                // 메시지 전송
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
-                socket.Send(sendBuff);
+                try
+                {
+                    // 서버 연결 시도
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"서버 {socket.RemoteEndPoint.ToString()}에 연결됨");
 
-                // 서버 응답 수신
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = socket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Server] {recvData}");
+                    // 메시지 전송
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    socket.Send(sendBuff);
 
-                // 연결 종료
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"클라이언트 오류: {e}");
+                    // 서버 응답 수신
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
+
+                    // 연결 종료
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"클라이언트 오류: {e}");
+                }
+
+                Thread.Sleep(1000);
             }
         }
     }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,6 +6,32 @@ namespace ServerCore
 {
     class Program
     {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 클라이언트 메시지 수신
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // 서버 응답 전송
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server !");
+                clientSocket.Send(sendBuff);
+
+                // 연결 종료
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"서버 오류: {e}");
+            }
+        }
+
         static void Main(string[] args)
         {
             // DNS (Domain Name System)에서 호스트 이름과 IP 주소를 가져오기
@@ -14,41 +40,13 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); // 포트 번호 7777
 
-            // 소켓 생성
-            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+             _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("서버가 클라이언트를 기다리고 있습니다...");
 
-            try
+
+            while (true)
             {
-                // 소켓 바인딩 및 대기 설정
-                listenSocket.Bind(endPoint);
-                listenSocket.Listen(10); // 최대 대기 연결 수 설정
-
-                Console.WriteLine("서버가 클라이언트를 기다리고 있습니다...");
-
-                while (true)
-                {
-                    // 클라이언트 연결 수락
-                    Socket clientSocket = listenSocket.Accept();
-                    Console.WriteLine("클라이언트 연결 수락됨");
-
-                    // 클라이언트 메시지 수신
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Client] {recvData}");
-
-                    // 서버 응답 전송
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server !");
-                    clientSocket.Send(sendBuff);
-
-                    // 연결 종료
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"서버 오류: {e}");
+                ;
             }
         }
     }
